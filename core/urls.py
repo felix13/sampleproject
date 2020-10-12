a58d95652f7e372bfe6f7e3b1aa2efb833729321
felix13 <29225465_rogitofelix_29225465>
2020-10-12 13:21:30 +0300

    added media and static path in url file side by side with settings file

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', include('uploads.urls')),
 ]
 
+
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
